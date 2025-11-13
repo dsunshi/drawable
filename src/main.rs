b761656f425ac8c561779d2b14e81287fc048ed3
D. Sunshine <david@sunshines.org>
2025-11-13 09:22:41 -0500

    Drawing Delaunay Triangulation

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,41 @@
 use macroquad::prelude::*;
 use macroquad::rand;
 
-use spade::Point2;
+use spade::{DelaunayTriangulation, Triangulation, Point2};
 
 const POINT_R: f32 = 2.0;
+const LINE_T:  f32 = 1.0;
+
+const NUM_POINTS: u32 = 100;
 
 fn draw_point(x: f32, y: f32, color: Color) {
     draw_circle(x, y, POINT_R, color);
 }
 
+fn p2v(p: Point2<f32>) -> Vec2 {
+    Vec2::new(p.x, p.y)
+}
+
 #[macroquad::main("drawable")]
 async fn main() {
-    let width  = screen_width() as f32;
+    let width  = screen_width()  as f32;
     let height = screen_height() as f32;
 
     // Setup
     // Optional random seed
     // rand::srand(macroquad::miniquad::date::now() as _);
     let mut points:Vec<Point2<f32>> = Vec::new();
-    for _i in 0..100 {
+    let mut triangulation: DelaunayTriangulation<_> = DelaunayTriangulation::new();
+
+    for _i in 0..NUM_POINTS {
         points.push(Point2::new(rand::gen_range(0.0, width),
                                 rand::gen_range(0.0, height)));
+    }
+
+    for point in &points {
+        _ = triangulation.insert(*point).map_err(|_err| {
+            eprintln!("Failed to insert point into triangulation!");
+        });
     }
 
     loop {
@@ -28,6 +43,14 @@ async fn main() {
 
         for point in &points {
             draw_point(point.x, point.y, BLACK);
+        }
+
+        for face in triangulation.inner_faces() {
+          let vertices = face.vertices();
+          let a = p2v(vertices[0].position());
+          let b = p2v(vertices[1].position());
+          let c = p2v(vertices[2].position());
+          draw_triangle_lines(a, b, c, LINE_T, BLACK);
         }
 
         next_frame().await
