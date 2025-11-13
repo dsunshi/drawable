5b1adc27648a3c4f8429d9d8481c938346232fca
D. Sunshine <david@sunshines.org>
2025-11-13 15:49:27 -0500

    Investigating issue with draw_shape

--- src/main.rs
+++ src/main.rs
@@ -169,37 +169,52 @@ async fn main() {
         //   draw_triangle_lines(a, b, c, LINE_T, BLACK);
         // }
         
-        // for face in &voronoi_faces {
-        //     draw_shape_lines(face, BLACK);
-        // }
-        for edge in triangulation.undirected_voronoi_edges() {
-            match edge.vertices() {
-                [Inner(from), Inner(to)] => {
-                    // "from" and "to" are inner faces of the Delaunay triangulation
-                    println!(
-                        "Found voronoi edge between {:?} and {:?}",
-                        from.circumcenter(),
-                        to.circumcenter()
-                    );
-                    let start = as_vec(from.circumcenter());
-                    let end   = as_vec(to.circumcenter());
-                    draw_line(start.x, start.y, end.x, end.y, LINE_T, BLACK);
 
-                }
-                [Inner(from), Outer(edge)] | [Outer(edge), Inner(from)] => {
-                    // Some lines don't have a finite end and extend into infinity.
-                    println!(
-                        "Found infinite voronoi edge going out of {:?} into the direction {:?}",
-                        from.circumcenter(),
-                        edge.direction_vector()
-                    );
-                }
-                [Outer(_), Outer(_)] => {
-                    // This case only happens if all vertices of the triangulation lie on the
-                    // same line and can probably be ignored.
+        for face in triangulation.voronoi_faces() {
+            // println!("found a face!");
+            // let mut shape: Vec<Vec2> = Vec::new(); 
+            let edges = face.adjacent_edges();
+            for edge in edges {
+                let from = edge.from().position();
+                let to = edge.to().position();
+                if let Some(start) = from {
+                    if let Some(end) = to {
+                        draw_line(start.x, start.y, end.x, end.y, LINE_T, BLACK);
+                    }
                 }
             }
         }
+        // for face in &voronoi_faces {
+        //     draw_shape_lines(face, BLACK);
+        // }
+        // for edge in triangulation.undirected_voronoi_edges() {
+        //     match edge.vertices() {
+        //         [Inner(from), Inner(to)] => {
+        //             // "from" and "to" are inner faces of the Delaunay triangulation
+        //             println!(
+        //                 "Found voronoi edge between {:?} and {:?}",
+        //                 from.circumcenter(),
+        //                 to.circumcenter()
+        //             );
+        //             let start = as_vec(from.circumcenter());
+        //             let end   = as_vec(to.circumcenter());
+        //             draw_line(start.x, start.y, end.x, end.y, LINE_T, BLACK);
+        //
+        //         }
+        //         [Inner(from), Outer(edge)] | [Outer(edge), Inner(from)] => {
+        //             // Some lines don't have a finite end and extend into infinity.
+        //             println!(
+        //                 "Found infinite voronoi edge going out of {:?} into the direction {:?}",
+        //                 from.circumcenter(),
+        //                 edge.direction_vector()
+        //             );
+        //         }
+        //         [Outer(_), Outer(_)] => {
+        //             // This case only happens if all vertices of the triangulation lie on the
+        //             // same line and can probably be ignored.
+        //         }
+        //     }
+        // }
 
         next_frame().await
     }
