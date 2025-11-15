9413aa1d9595ded6e3d32b63b99e001feadbc4ca
D. Sunshine <david@sunshines.org>
2025-11-14 22:27:03 -0500

    Initial prototype of new backend

--- src/main.rs
+++ src/main.rs
@@ -1,268 +1,167 @@
 use macroquad::prelude::*;
-// use macroquad::rand;
-//
-// // use spade::{DelaunayTriangulation, Point2, Triangulation};
-//
-// use drawable::gcode::*;
-//
-// // use image::*;
-//
-// const POINT_R:   f32 = 2.0;
-// const LINE_T:    f32 = 1.0;
-// const LERP_RATE: f32 = 0.01;
-// const LUM_LIMIT: f32 = 100.0;
-//
-// const NUM_POINTS: u32 = 10000;
-//
-// const IMG_IN: &str = "bird.jpg";
 
-// fn load_points(filename: &str) -> Vec<Point2<f32>> {
-//     let mut points: Vec<Point2<f32>> = Vec::new();
-//
-//     let image = image::open(filename).unwrap(); // TODO
-//     let (width, height) = image.dimensions();
-//
-//     loop {
-//         let x = rand::gen_range(0.0, width as f32);
-//         let y = rand::gen_range(0.0, height as f32);
-//
-//         let pixel = image.get_pixel(x as u32, y as u32);
-//         let r = pixel[0] as f32;
-//         let g = pixel[1] as f32;
-//         let b = pixel[2] as f32;
-//
-//         let lum = 0.299 * r + 0.587 * g + 0.114 * b;
-//         // let lum = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-//         // let lum = (r + g + b) / 3;
-//
-//         if rand::gen_range(0.0, LUM_LIMIT) > lum {
-//             points.push(Point2::new(x, y));
-//         }
-//
-//         if points.len() >= NUM_POINTS as usize { break; }
-//     }
-//
-//     points
-// }
-//
-// struct Polygon {
-//     pub points:   Vec<Point2<f32>>,
-//     pub seed:     Option<Point2<f32>>,
-//     pub enclosed: bool,
-// }
-//
-// impl Polygon {
-//     pub fn new() -> Self {
-//         Polygon {
-//             points:   Vec::new(),
-//             seed:     None,
-//             enclosed: false,
-//         }
-//     }
-//
-//     fn lerp(v0: f32, v1: f32, t: f32) -> f32 {
-//       (1.0 - t) * v0 + t * v1
-//     }
-//
-//     pub fn relax(&mut self) {
-//         if self.enclosed {
-//             if let Some(seed) = self.seed {
-//                 let c = self.centroid();
-//                 let x = Self::lerp(seed.x, c.x, LERP_RATE);
-//                 let y = Self::lerp(seed.y, c.y, LERP_RATE);
-//
-//                 self.seed = Some(Point2::new(x, y));
-//             }
-//         }
-//     }
-//
-//     pub fn centroid(&self) -> Point2<f32> {
-//         let mut c: Point2<f32> = Point2::new(0.0, 0.0);
-//
-//         let n = self.points.len();
-//         let mut area = 0.0;
-//
-//         for i in 0..n {
-//             let v0 = self.points[i];
-//             let v1 = self.points[(i + 1) % n];
-//             let cross_p = v0.x * v1.y - v1.x * v0.y;
-//
-//             area += cross_p;
-//             c.x += (v0.x + v1.x) * cross_p;
-//             c.y += (v0.y + v1.y) * cross_p;
-//         }
-//
-//         area /= 2.0;
-//         c.x  /= 6.0 * area;
-//         c.y  /= 6.0 * area;
-//
-//         c
-//     }
-// }
-//
-// fn draw_point(x: f32, y: f32, color: Color) {
-//     draw_circle(x, y, POINT_R, color);
-// }
-//
-// // fn as_vec(p: Point2<f32>) -> Vec2 {
-// //     Vec2::new(p.x, p.y)
-// // }
-//
-// fn draw_polygon_lines(polygon: &Polygon, color: Color) {
-//     if polygon.enclosed {
-//         let points = &polygon.points;
-//         let n = points.len() - 1;
-//
-//         for i in 0..n {
-//             draw_line(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y, LINE_T, color);
-//         }
-//         draw_line(points[n].x, points[n].y, points[0].x, points[0].y, LINE_T, RED);
-//     }
-// }
-//
-// fn point_dist(a: Point2<f32>, b: &Point2<f32>) -> f32 {
-//     ((a.x - b.x).powi(2) + (a.y - b.y).powi(2)).sqrt()
-// }
-//
-// fn nearest_point(c: Point2<f32>, points: &Vec<Point2<f32>>) -> Option<Point2<f32>> {
-//     let mut min_dist: f32 = f32::MAX;
-//     let mut nearest: Option<Point2<f32>> = None;
-//
-//     for point in points {
-//         let dist = point_dist(c, point);
-//         if dist < min_dist {
-//             min_dist = dist;
-//             nearest  = Some(*point);
-//         }
-//     }
-//
-//     nearest
-// }
-//
-// fn update_points(polygons: &Vec<Polygon>, points: &mut Vec<Point2<f32>>) {
-//     points.clear();
-//     for polygon in polygons {
-//         if let Some(seed) = polygon.seed {
-//             points.push(seed);
-//         }
-//     }
-// }
-//
-// fn voronoi_polygons(triangulation: &DelaunayTriangulation<Point2<f32>>, seed_points: &Vec<Point2<f32>>) -> Vec<Polygon> {
-//     let mut polygons: Vec<Polygon> = Vec::new();
-//
-//     for face in triangulation.voronoi_faces() {
-//         let mut polygon = Polygon::new();
-//         let edges = face.adjacent_edges();
-//
-//         let mut enclosed: bool = true;
-//         for edge in edges {
-//             let from = edge.from().position();
-//             let to   = edge.to().position();
-//
-//             if let Some(start) = from {
-//                 if let Some(end) = to {
-//                     polygon.points.push(end);
-//                     polygon.points.push(start);
-//                 } else {
-//                     enclosed = false;
-//                 }
-//             } else if let Some(_end) = to {
-//                 enclosed = false;
-//             }
-//         }
-//
-//         polygon.seed = nearest_point(polygon.centroid(), seed_points);
-//         if enclosed && polygon.points.len() > 0 {
-//             polygon.enclosed = true;
-//         }
-//
-//         polygons.push(polygon);
-//     }
-//
-//     polygons
-// }
+use d3_delaunay_rs::delaunay::Delaunay;
+use geo_types::Coord;
+
+use std::collections::HashMap;
+
+use ::rand::prelude::*;
+
+use image::*;
+
+const IMG_IN: &str = "bird.jpg";
+
+const POINT_R:   f32 = 2.0;
+const LINE_T:    f32 = 1.0;
+
+pub fn draw_point(x: f32, y: f32, color: Color) {
+    draw_circle(x, y, POINT_R, color);
+}
+
+pub struct Stipple {
+    width: usize,
+    height: usize,
+    n: usize,
+    data: Vec<f64>,
+    pub delaunay: Delaunay<f64>,
+}
+
+impl Stipple {
+    pub(crate) fn new( width: usize, height: usize,
+        data: Vec<f64>, // gray scale - image data
+        n: usize,
+    ) -> Stipple {
+        let mut h_points: HashMap<usize, Coord<f64>> = HashMap::with_capacity(n);
+        // Initialize the points using rejection sampling.
+        let mut rng = ::rand::rng();
+        for i in 0..n {
+            '_30Loop: for _ in 0..30 {
+                let x = rng.random_range(0.0..(width as f64));
+               let y = rng.random_range(0.0..(height as f64));
+                let index = y as usize * width + x as usize;
+                h_points.insert(i, Coord { x, y });
+
+                if index > width * height {
+                    println!("({}, {})", x, y);
+                }
+
+                if rng.random_range(0.0..1.0) < data[index] {
+                    break '_30Loop;
+                }
+            }
+        }
+
+        let points   = h_points.into_values().collect::<Vec<_>>();
+        let delaunay = Delaunay::new(&points);
+
+        Stipple {
+            width,
+            height,
+            n,
+            data,
+            delaunay,
+        }
+    }
+
+    pub fn next(&mut self, k: usize) {
+        // Compute the weighted centroid for each Voronoi cell.
+        let mut c: Vec<Coord<f64>> = Vec::with_capacity(self.n);
+        let mut s: Vec<f64> = Vec::with_capacity(self.n);
+        for _i in 0..self.n {
+            c.push(Coord { x: 0_f64, y: 0_f64 });
+            s.push(0_f64);
+        }
+
+        let mut i = 0;
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let w = self.data[y * self.width + x];
+                i = self.delaunay.find(
+                    &Coord {
+                        x: x as f64 + 0.5_f64,
+                        y: y as f64 + 0.5_f64,
+                    },
+                    Some(i),
+                );
+                s[i] += w;
+                c[i].x += w * (x as f64 + 0.5_f64);
+                c[i].y += w * (y as f64 + 0.5_f64);
+            }
+        }
+
+        // Relax the diagram by moving points to the weighted centroid.
+        // Wiggle the points a little bit so they don’t get stuck.
+        let w = (k as f64 + 1_f64).powf(-0.8) * 10_f64;
+        for i in 0..self.n {
+            let x0 = self.delaunay.points[i].x;
+            let y0 = self.delaunay.points[i].y;
+            let x1 = if s[i] == 0_f64 { x0 } else { c[i].x / s[i] };
+            let y1 = if s[i] == 0_f64 { y0 } else { c[i].y / s[i] };
+
+            self.delaunay.points[i].x =
+                x0 + (x1 - x0) * 1.8 + (::rand::rng().random_range(0.0..1.0) - 0.5) * w;
+            self.delaunay.points[i].y =
+                y0 + (y1 - y0) * 1.8 + (::rand::rng().random_range(0.0..1.0) - 0.5) * w;
+        }
+
+        // // TODO: doing a update() the hard way...
+        // // What can I refactor here.
+        self.delaunay = Delaunay::new(&self.delaunay.points);
+
+    }
+}
+
+pub fn as_bytes(img: &DynamicImage) -> Vec<f64> {
+    let (width, height)     = img.dimensions();
+    // let mut bytes: Vec<f64> = Vec::with_capacity((width * height) as usize);
+    let mut bytes: Vec<f64> = vec![0.0; (width * height) as usize];
+
+    for x in 0..width {
+        for y in 0..height {
+
+            let pixel = img.get_pixel(x as u32, y as u32);
+            let index = (y * width + x) as usize;
+
+            if index >= (width * height) as usize {
+                println!("({}, {})", x, y);
+            }
+
+            let r = pixel[0] as f32;
+            let g = pixel[1] as f32;
+            let b = pixel[2] as f32;
+
+            // let lum = 0.299 * r + 0.587 * g + 0.114 * b;
+            let lum = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+            // let lum = (r + g + b) / 3;
+
+            bytes[index] = lum as f64;
+        }
+    }
+
+    bytes
+}
 
 #[macroquad::main("drawable")]
 async fn main() {
-    // let image = image::open(IMG_IN).unwrap(); // TODO
-    // let (width, height) = image.dimensions();
-    //
-    // request_new_screen_size(width as f32, height as f32);
-    let width  = screen_width()  as f32;
-    let height = screen_height() as f32;
+    let image = image::open(IMG_IN).unwrap(); // TODO
+    let (width, height) = image.dimensions();
+    println!("Image size: {} x {}", width, height);
+    
+    let mut stipple = Stipple::new(width as usize, height as usize, as_bytes(&image), 10000);
 
-    // let mut gcode = Printer::new((50.0, 35.0), (254.0, 212.0), PrintMode::DOTS);
-    // gcode.set_scale(width, height);
-    //
-    // // Setup
-    // // Optional random seed
-    // // rand::srand(macroquad::miniquad::date::now() as _);
-    // // let mut points:Vec<Point2<f32>> = Vec::new();
-    // //
-    // // for _i in 0..NUM_POINTS {
-    // //     points.push(Point2::new(rand::gen_range(0.0, width),
-    // //                             rand::gen_range(0.0, height)));
-    // // }
-    // let mut points = load_points(IMG_IN);
-    //
-    // let mut triangulation: DelaunayTriangulation<_> = DelaunayTriangulation::new();
-    // for point in &points {
-    //     _ = triangulation.insert(*point).map_err(|_err| {
-    //         eprintln!("Failed to insert point into triangulation!");
-    //     });
-    //
-    //     // gcode test
-    //     gcode.goto(point.x, point.y);
-    // }
-    //
-    // let mut polygons = voronoi_polygons(&triangulation, &points);
-    //
-    // gcode.save("drawing.gcode");
+    request_new_screen_size(width as f32, height as f32);
+
+    // let width  = screen_width()  as f32;
+    // let height = screen_height() as f32;
 
     loop {
-        // let mut triangulation: DelaunayTriangulation<_> = DelaunayTriangulation::new();
-        //
-        // for point in &points {
-        //     _ = triangulation.insert(*point).map_err(|_err| {
-        //         eprintln!("Failed to insert point into triangulation!");
-        //     });
-        // }
-        //
-        // let mut polygons = voronoi_polygons(&triangulation, &points);
-
         clear_background(WHITE);
 
-        // for point in &points {
-        //     draw_point(point.x, point.y, BLACK);
-        // }
+        for point in &stipple.delaunay.points {
+            draw_point(point.x as f32, point.y as f32, BLACK);
+        }
 
-        // for polygon in &polygons {
-        //     // draw_polygon_lines(polygon, BLACK);
-        //     let p = polygon.centroid();
-        //     if let Some(seed) = polygon.seed {
-        //         if polygon.enclosed {
-        //             draw_point(seed.x, seed.y, BLACK);
-        //         } else {
-        //             draw_point(p.x, p.y, RED);
-        //             draw_point(seed.x, seed.y, BLUE);
-        //         }
-        //     }
-        // }
-        //
-        // for polygon in &mut polygons {
-        //     polygon.relax();
-        // }
-        //
-        // update_points(&polygons, &mut points);
-
-        // for face in triangulation.inner_faces() {
-        //   let vertices = face.vertices();
-        //   let a = as_vec(vertices[0].position());
-        //   let b = as_vec(vertices[1].position());
-        //   let c = as_vec(vertices[2].position());
-        //   draw_triangle_lines(a, b, c, LINE_T, BLACK);
-        // }
+        stipple.next(10);
 
         next_frame().await
     }
